ed186a22755c6d5e2a92efa0f0b15fa750ce2687
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-10-26 10:51:08 -0700

    added database models

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,34 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+--SYNTAX TO CREATE CATEGORY
+CREATE TABLE category (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ category_name VARCHAR(255) NOT NULL
+) 
+
+--SYNTAX TO CREATE PRODUCT
+CREATE TABLE product (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    price DECIMAL (5,2) NOT NULL, -- HOW TO VAILDATE IS DECIMAL
+    stock INT NOT NULL DEFAULT 10--HOW TO VALIDATE IS NUMERIC
+    category_id INT NOT NULL,
+      FOREIGN KEY (category_id) references category(id)
+)
+
+--SYNTAX TO CREATE TAG
+CREATE TABLE tag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255)
+)
+
+--SYNTAX PRODUCT TAG
+CREATE TABLE Product_tag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+        FOREIGN KEY (product_id) references product(id)
+    tag_id INT,
+        FOREIGN KEY (tag_id) references tag(id)
+        
+)
